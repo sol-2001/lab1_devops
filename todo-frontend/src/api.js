8601af7de246b84d92c2a69d31eef809c18852c0
Danil <labii@niuitmo.ru>
2024-12-21 01:48:13 +0300

    correct api.js_v2

--- todo-frontend/src/api.js
+++ todo-frontend/src/api.js
@@ -1,27 +1,29 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 export async function getTasks() {
-    const resp = await fetch('http://84.252.131.15:8080/tasks');
-    return resp.json();
-  }
-  
-  export async function createTask(title) {
-    const resp = await fetch('http://84.252.131.15:8080/tasks', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({ title })
-    });
-    return resp.json();
-  }
-  
-  export async function updateTask(id, title, completed) {
-    const resp = await fetch(`http://84.252.131.15:8080/tasks/${id}`, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({ title, completed })
-    });
-    return resp.json();
-  }
-  
-  export async function deleteTask(id) {
-    await fetch(`http://84.252.131.15:8080/tasks/${id}`, { method: 'DELETE' });
-  }
-  
+  const resp = await fetch(`${API_URL}/tasks`);
+  return resp.json();
+}
+
+export async function createTask(title) {
+  const resp = await fetch(`${API_URL}/tasks`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title }),
+  });
+  return resp.json();
+}
+
+export async function updateTask(id, title, completed) {
+  const resp = await fetch(`${API_URL}/tasks/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title, completed }),
+  });
+  return resp.json();
+}
+
+export async function deleteTask(id) {
+  await fetch(`${API_URL}/tasks/${id}`, { method: 'DELETE' });
+}
+
