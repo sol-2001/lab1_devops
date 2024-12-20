7c996deae369a5215921e3fb2535f4e9b95fc6ca
Danil <labii@niuitmo.ru>
2024-12-21 00:57:47 +0300

    correct api.js

--- todo-frontend/src/api.js
+++ todo-frontend/src/api.js
@@ -1,10 +1,10 @@
 export async function getTasks() {
-    const resp = await fetch('http://localhost:8080/tasks');
+    const resp = await fetch('http://84.252.131.15:8080/tasks');
     return resp.json();
   }
   
   export async function createTask(title) {
-    const resp = await fetch('http://localhost:8080/tasks', {
+    const resp = await fetch('http://84.252.131.15:8080/tasks', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ title })
@@ -13,7 +13,7 @@ export async function getTasks() {
   }
   
   export async function updateTask(id, title, completed) {
-    const resp = await fetch(`http://localhost:8080/tasks/${id}`, {
+    const resp = await fetch(`http://84.252.131.15:8080/tasks/${id}`, {
       method: 'PUT',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ title, completed })
@@ -22,6 +22,6 @@ export async function getTasks() {
   }
   
   export async function deleteTask(id) {
-    await fetch(`http://localhost:8080/tasks/${id}`, { method: 'DELETE' });
+    await fetch(`http://84.252.131.15:8080/tasks/${id}`, { method: 'DELETE' });
   }
   
