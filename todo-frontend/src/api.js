c4569e054d36902c84db4fdfe3f8008e6e4531f6
Danil <labii@niuitmo.ru>
2024-12-21 14:09:16 +0300

    test_3

--- todo-frontend/src/api.js
+++ todo-frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL || 'http://backend:8080';
 
 export async function getTasks() {
   const resp = await fetch(`${API_URL}/tasks`);
