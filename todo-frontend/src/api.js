29984fb86fea5c3fbb0c8c44cf1de1018ca10a7b
Danil <labii@niuitmo.ru>
2025-01-19 20:46:42 +0300

    19.01.2025_3

--- todo-frontend/src/api.js
+++ todo-frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://89.169.128.169:8081' || 'http://backend:8081';
+const API_URL =  'http://158.160.151.201:8081' || 'http://89.169.128.169:8081';
 
 export async function getTasks() {
   const resp = await fetch(`${API_URL}/tasks`);
