6b007a76976bec93576d4e24b93fc11a0ed49ad2
Danil <labii@niuitmo.ru>
2025-01-27 18:13:29 +0300

    init

--- todo-backend/src/test/java/com/example/todo_backend/TodoBackendApplicationMainTest.java
+++ todo-backend/src/test/java/com/example/todo_backend/TodoBackendApplicationMainTest.java
@@ -1,7 +1,11 @@
 package com.example.todo_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest(properties = {"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"})
+@ActiveProfiles("test")
 public class TodoBackendApplicationMainTest {
 
     @Test
