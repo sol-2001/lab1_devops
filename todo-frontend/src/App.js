64f7373ed0372c866b2b031b1ed722bcd2da8429
Danil <labii@niuitmo.ru>
2025-01-29 16:06:36 +0300

    init

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
     <div style={{ margin: "20px" }}>
       <h1>Todo List</h1>
       <div>
-        <input value={newTitle} onChange={e => setNewTitle(e.target.value)} placeholder="Create new task"/>
+        <input value={newTitle} onChange={e => setNewTitle(e.target.value)} placeholder="Create task"/>
         <button onClick={addTask}>Add</button>
       </div>
       <ul>
